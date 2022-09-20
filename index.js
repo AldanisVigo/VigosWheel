b28232570c357e27edf3184d72ea3b9991125b4c
Aldanis Vigo <aldanisvigo@gmail.com>
2022-09-20 08:55:54 -0500

    Attempting to detect angle it lands on

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const startingPoint = {
 
 const numberOfPeople = 10;
 const lineLength = 100;
-const numberDistance = 120;
+const numberDistance = 136;
 let incrementAngle = 2 * Math.PI / numberOfPeople
 let incrementAngleDeg = 360 / numberOfPeople
 wheel.style.position = 'relative'
@@ -20,27 +20,24 @@ let originOffset = {
     y : 100
 }
 
-let ranges = new Map()
 for(let i = 0; i < numberOfPeople; i++){
     drawingPath += `M ${originOffset.x},${originOffset.y}`
-    let lineToX = Number.parseFloat((Math.cos(incrementAngle * i - 1) * lineLength) + originOffset.x)
-    let numToX = Number.parseFloat((Math.cos(incrementAngle * i) * numberDistance) + originOffset.x) //- (2 * (Math.PI/180)))
+    let lineToX = Number.parseFloat((Math.cos(incrementAngle * i) * lineLength) + originOffset.x)
+    let lineToY = Number.parseFloat((Math.sin(incrementAngle * i) * lineLength) + originOffset.y)
 
-    let lineToY = Number.parseFloat((Math.sin(incrementAngle * i - 1) * lineLength) + originOffset.y)
+    let numToX = Number.parseFloat((Math.cos(incrementAngle * i) * numberDistance) + originOffset.x) //- (2 * (Math.PI/180)))
     let numToY = Number.parseFloat((Math.sin(incrementAngle * i) * numberDistance) + originOffset.y) //- (2 * (Math.PI/180)))
 
     drawingPath += `L ${lineToX},${lineToY} `
 
-    ranges.set(i,(360 / numberOfPeople) * (i + 1))
-
-
     //create the number
     let num = document.createElement('div')
     num.style.position = 'absolute'
-    num.style.left = numToX - 2 + "px"
+    num.style.left = numToX - 20 + "px"
     num.style.top = numToY - 8 + "px"
     num.style.transform = `rotateY(${incrementAngleDeg}deg)`
-    num.innerText = i + 1
+    // num.innerText = i + 1// + ' ' + Number(incrementAngleDeg * i) + 'deg'
+    num.innerText = Number(incrementAngleDeg * i) + 'deg'
     num.style.fontWeight = 'bolder'
     wheel.appendChild(num)
 }
@@ -51,44 +48,16 @@ wheelpath.setAttribute('d',drawingPath)
 
 const spinButton = document.querySelector('#spinbutton')
 const landedOn = document.querySelector('#landedon')
+let lastSpinVal = 0;
 spinButton.addEventListener('click',e=>{
     console.log("Spin")
-    wheel.style.animation = 'rotate 2s linear 2s infinite'
-    let randomTime = (Math.random() * 25) + 5 * 1000
-    console.log(randomTime)
-    wheel.style.animationPlayState = 'running'
-    setTimeout(()=>{
-        wheel.style.animationPlayState = 'paused'
-        let tr = window.getComputedStyle(wheel).transform
-        if( tr !== 'none') {
-            var values = tr.split('(')[1]
-            values = values.split(')')[0]
-            values = values.split(',')
-            var a = values[0]
-            var b = values[1]
-            //var c = values[2]
-            //var d = values[3]
-            var scale = Math.sqrt(a*a + b*b)
-            var radians = Math.atan2(b, a);
-            if ( radians < 0 ) {
-                radians += (2 * Math.PI);
-            }
-            var angle = Math.round( radians * (180/Math.PI));
-        } else {
-            var angle = 0;
-        }
-
-        console.log('Rotate: ' + angle + 'deg');
-        let landed;
-        for(let j = 0; j < numberOfPeople; j++){
-            console.log(ranges.get(j))
-            console.log(angle)
-            if(angle < ranges.get(j)){
-                landed = j
-                break
-            }
-        }
-        landedOn.innerText = "Landed on " + Number(landed + 1)
-
-    },randomTime)
+    // wheel.style.animation = 'rotate 2s linear 2s infinite'
+    let randomAngle = (Math.random() * 360 * 20) + 1
+    lastSpinVal = randomAngle
+    console.log(`Rotating to: ${lastSpinVal}`)
+    wheel.style.transform = `rotateZ(${lastSpinVal}deg)`
+    while(lastSpinVal > 360){
+        lastSpinVal -= 360
+    }
+    console.log(`Rotating to: ${360 - lastSpinVal}`)
 })
